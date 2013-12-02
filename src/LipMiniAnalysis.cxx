3890b1055f4a16ae9859ec7ba0bee9c10c353798
ampereira <lazeroth89@gmail.com>
2013-12-02 16:44:53 +0000

    ds

--- src/LipMiniAnalysis.cxx
+++ src/LipMiniAnalysis.cxx
@@ -1791,13 +1791,13 @@ void LipMiniAnalysis::Loop() {
 
 	// start loop over all events
 	int max = 0;
-	#pragma omp parallel reduction(+:max) num_threads(NUM_THREADS)
-	//#pragma omp parallel num_threads(NUM_THREADS)
+	//#pragma omp parallel reduction(+:max) num_threads(NUM_THREADS)
+	#pragma omp parallel num_threads(NUM_THREADS)
 	{
 		// If a thread has reached the end of the file it cancels the loop
 		#pragma omp for// schedule(dynamic)
 		for (unsigned i_event = 0; i_event < MAX_EVENTS; ++i_event) {
-			int tid = 0;//omp_get_thread_num();
+			int tid = omp_get_thread_num();
 			Int_t ientry;
 			// standard stuff to get the event in memory
 			//#pragma omp critical
