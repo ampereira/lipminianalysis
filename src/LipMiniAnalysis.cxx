d99cd873fc96cff91d5213e969d5028aa74e0039
ampereira <lazeroth89@gmail.com>
2014-05-15 14:42:49 +0100

    dsa

--- src/LipMiniAnalysis.cxx
+++ src/LipMiniAnalysis.cxx
@@ -683,7 +683,7 @@ void LipMiniAnalysis::Start(int i_argc, char *const *i_argv) {
   sprintf(ErrName, "%s%s.err", SystName[0].c_str(), OutputFileName.c_str());
 
   // .....comment the two lines below if you would like to have the output writen to screen
-  freopen(LogName, "w", stdout);
+  //freopen(LogName, "w", stdout);
   //freopen(ErrName, "w", stderr);
 
   // Prints logo
